d760920da125b641c50c99dd2531a09ed3b4ef53
MaxMozgunov <136356747+MaxMozgunov@users.noreply.github.com>
2024-05-04 15:58:15 +0200

    I have mistake

--- board/database.py
+++ board/database.py
@@ -6,7 +6,9 @@ from flask import current_app, g
 
 
 def init_app(app):
+    app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
+
 
 @click.command("init-db")
 def init_db_command():
@@ -24,3 +26,8 @@ def get_db():
         )
         g.db.row_factory = sqlite3.Row
     return g.db
+
+def close_db(e=None):
+    db = g.pop("db", None)
+    if db is not None:
+        db.close()
